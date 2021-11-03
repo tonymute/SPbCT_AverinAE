e1473c3e8db918681c81c93c074df852cbd692d1
tonymute <74365404+tonymute@users.noreply.github.com>
2021-11-03 11:53:04 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    FILE* ptrFile = fopen(argv[1], "r");
+    int c = fgetc(ptrFile);
+    std::string s = "";
+
+    while (c != EOF)
+    {
+        s += c;
+        s += ' ';
+        c = fgetc(ptrFile);
+    }
+
+    fclose(ptrFile);
+    std::cout << "\"" << s << "\"";
+
+    return 0;
+}
